523ab8d0a5342d237baf0d4c03374c3da0225845
Drashti Gadhia <drashtibgadhia10@gmail.com>
2019-10-23 17:43:45 +0200

    base crated for quiz app

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-nav tabs>
+    <b-nav-item disabled>Question Quiz</b-nav-item>
+    <b-nav-item disabled>Counter: 2/10</b-nav-item>
+  </b-nav>
+  </div>
+</template>
+
+<style scoped>
+.nav{
+  font-weight: 500;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h4>Category : Computer Science</h4>
+     <b-jumbotron class="question-box-container">
+
+    <template v-slot:lead>
+     Question
+    </template>
+    <hr class="my-4">
+    <p>
+     list of answers
+    </p>
+    <b-button variant="success" href="#">Submit</b-button>
+      <b-button variant="primary" href="#">Next</b-button>
+  </b-jumbotron>
+
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    fetch("https://opentdb.com/api.php?amount=10&category=18&type=multiple",)
+  }
+}
+</script>
+
+<style scoped>
+h4{
+  margin: 15px;
+}
+</style>
